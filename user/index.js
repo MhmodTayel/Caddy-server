e6df12136391d4726aa092bdf3356dde67fac2db
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2022-02-20 13:02:49 +0200

    add user

--- user/index.js
+++ user/index.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
   res.status(403).json(err);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Connection Started on port ${PORT}`);
 });
