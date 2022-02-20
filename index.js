cef678a630a86cc603e6e484466325cb4830ed5a
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2022-02-20 11:28:30 +0200

    modifiy caddy

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>This is user website</h1>");
+  res.send("<h1>This is ADMIN website</h1>");
 });
 
 app.listen(port, () => {
