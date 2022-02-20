2eb13a3d91427d643cf40e9c640cb03a4bccabd2
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2022-02-20 11:13:35 +0200

    modifiy caddy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("production will be fine v5!");
+  res.send("production will be fine v6!");
 });
 
 app.listen(port, () => {
