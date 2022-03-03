997534a3ac60b6eef24f566cd5034089343add75
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2022-03-03 09:13:17 +0200

    semi production

--- user/index.js
+++ user/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 var cors = require("cors");
 const app = express()
 const port = 4000
+var server = require("https").createServer(app);
+
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
