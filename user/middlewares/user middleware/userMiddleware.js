e9f6ad6d150288bbcb13dff47474de2dcddcdf85
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2022-03-08 18:19:59 +0200

    test

--- user/middlewares/user middleware/userMiddleware.js
+++ user/middlewares/user middleware/userMiddleware.js
@@ -3,8 +3,9 @@ const User = require("../../models/pharmacist");
 
 const userAuth = async (req, res, next) => {
 
-  if ((req.method == "POST"||req.method == "GET") &&(req.url == '/users/user/register'|| req.url == '/users/user/login' ||
-   req.url == "/users/contactUs" || req.url == '/users/notification' ||req.url == '/users/notification/all')) {
+  console.log(req.url,req.headers)
+  if (((req.method == "POST") &&(req.url == '/users/user/register'|| req.url == '/users/user/login' ||
+   req.url == "/users/contactUs")) || req.url.includes('static')) {
 
     next();
     return;
